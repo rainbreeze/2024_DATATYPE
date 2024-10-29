829b84a61f2543a9c05ab14ef976d9875afbfaab
joji <rainbreeze2@naver.com>
2024-10-29 15:13:31 +0900

    첫 커밋

---
+++ LECTURE/LEC_08/heap.py
@@ -0,0 +1,55 @@
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def insert(self, val):
+        self.heap.append(val)
+        self._heapify_up(len(self.heap) - 1)
+
+    def _heapify_up(self, index):
+        parent_index = (index - 1) // 2
+        if index > 0 and self.heap[index] < self.heap[parent_index]:
+            self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
+            self._heapify_up(parent_index)
+
+    def extract_min(self):
+        if len(self.heap) == 0:
+            raise IndexError("Heap is empty")
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        
+        min_val = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._heapify_down(0)
+        return min_val
+
+    def _heapify_down(self, index):
+        smallest = index
+        left = 2 * index + 1
+        right = 2 * index + 2
+
+        if left < len(self.heap) and self.heap[left] < self.heap[smallest]:
+            smallest = left
+        if right < len(self.heap) and self.heap[right] < self.heap[smallest]:
+            smallest = right
+        if smallest != index:
+            self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
+            self._heapify_down(smallest)
+
+    def peek(self):
+        if len(self.heap) == 0:
+            raise IndexError("Heap is empty")
+        return self.heap[0]
+
+    def is_empty(self):
+        return len(self.heap) == 0
+
+# 사용 예제
+min_heap = MinHeap()
+min_heap.insert(5)
+min_heap.insert(3)
+min_heap.insert(8)
+min_heap.insert(1)
+
+print(min_heap.extract_min())  # 1
+print(min_heap.peek())          # 3
